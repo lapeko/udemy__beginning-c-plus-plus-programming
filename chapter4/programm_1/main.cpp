173313d85f25bcb727b2d0eabb44ee810b9ad33a
lapeko <lapeko88@gmail.com>
2023-09-20 22:48:53 +0200

    create first c++ programm

---
+++ chapter4/programm_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int yourFavoriteNumber;
+    cout << "Enter your favorite number" << endl;
+    cin >> yourFavoriteNumber;
+    cout << "Your favorite number is: " << yourFavoriteNumber << endl;
+    return 0;
+}
