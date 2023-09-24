24251951d0a77c3be387f25ccbc65d673aa8f5c1
lapeko <lapeko88@gmail.com>
2023-09-24 14:04:35 +0200

    learn more  about vectors

--- chapter7/2-vectors/main.cpp
+++ chapter7/2-vectors/main.cpp
@@ -3,18 +3,29 @@
 
 using namespace std;
 
+void printMatrix(vector<vector<int>> matrix) {
+    cout << "matrix size: " << matrix.size() << endl;
+    for (const vector<int>& vec: matrix) {
+        cout << "inner vecor size: " << vec.size() << endl;
+        for (const int& item: vec) {
+            cout << item << " ";
+        }
+        cout << endl;
+    }
+    cout << "==================================================" << endl;
+}
+
 int main() {
     vector<vector<int>> matrix {
         {1, 2, 3},
         {4, 5, 6}
     };
     
-    for (const vector<int>& vec: matrix) {
-        for (const int& item: vec) {
-            cout << item << " ";
-        }
-        cout << endl;
-    }
+    printMatrix(matrix);
+    
+    matrix.at(0).push_back(4);
+    
+    printMatrix(matrix);
     
     return 0;
 }
