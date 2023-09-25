3a0a2f3092e060560c67b63f9ab9cfd743c27b28
lapeko <lapeko88@gmail.com>
2023-09-25 04:46:54 +0200

    boolalpha

---
+++ chapter8/2-bool-alpha/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << boolalpha;
+    cout << true << endl;
+    cout << noboolalpha;
+    cout << true << endl;
+    return 0;
+}
