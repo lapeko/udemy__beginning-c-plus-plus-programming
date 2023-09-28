cbc8e942c282f1d7110126572636bb9338e4bd4d
lapeko <lapeko88@gmail.com>
2023-09-28 22:46:20 +0200

    init chapter 11

---
+++ chapter11-functions/1-random/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    cout << "Max random: " << RAND_MAX << endl;
+    cout << "time(NULL) is " << time(NULL) << endl;
+    
+    srand(time(NULL));
+    
+    int min_random = 1;
+    int max_random = 5;
+    
+    for (int i {0}; i < 10; i++) cout << rand() % (max_random - min_random + 1) + min_random << endl;
+    
+    return 0;
+}
