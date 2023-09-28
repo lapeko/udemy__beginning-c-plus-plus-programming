e0738d83d68973385cee7a72d50a7d8aba97bd35
lapeko <lapeko88@gmail.com>
2023-09-28 22:50:11 +0200

    test arguments in function prototype

--- chapter11-functions/1-random/main.cpp
+++ chapter11-functions/1-random/main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+void test_prototype(int a);
+
 int main() {
     cout << "Max random: " << RAND_MAX << endl;
     cout << "time(NULL) is " << time(NULL) << endl;
@@ -16,4 +18,8 @@ int main() {
     for (int i {0}; i < 10; i++) cout << rand() % (max_random - min_random + 1) + min_random << endl;
     
     return 0;
+}
+
+void test_prototype(int b) {
+    cout << "It is b. Not a: " << b << endl;
 }
