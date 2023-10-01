46f87c9ab532ff40280a16262a354bde92a75cae
lapeko <lapeko88@gmail.com>
2023-10-02 00:30:28 +0200

    rename chapter6. learn dynamic memory allocation

---
+++ chapter12-pointers/2-dynamic-memory-allocation.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int size {};
+    int *arrPointer {nullptr};
+
+    cout << "Input size of the array: " << endl;
+    cin >> size;
+
+    arrPointer = new int [size];
+
+    for (int i = 0; i < size; i++) {
+        cout << "Enter a value for element: " << i << endl;
+        int newNum {};
+        cin >> newNum;
+        arrPointer[i] = newNum;
+    }
+
+    cout << "Your array is:" << endl;
+
+    for (int i = 0; i < size; i++) {
+        cout << "[" << i << "]" << ": " << arrPointer[i] << endl;
+    }
+
+    return 0;
+}
