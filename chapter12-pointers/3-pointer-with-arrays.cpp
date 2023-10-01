d4409a82c5de65dec96dac7da3e3a31021ab7595
lapeko <lapeko88@gmail.com>
2023-10-02 00:37:51 +0200

    access to values of array and array's ponter values

---
+++ chapter12-pointers/3-pointer-with-arrays.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int arr[] {1, 2, 3, 4};
+    int *arrPtr {arr};
+
+    cout << "arr[0]: " << arr[0] << endl;
+    cout << "arr[1]: " << arr[1] << endl;
+    cout << "arr[2]: " << arr[2] << endl;
+    cout << "arr[3]: " << arr[3] << endl;
+
+    cout << "arrPtr[0]: " << arrPtr[0] << endl;
+    cout << "arrPtr[1]: " << arrPtr[1] << endl;
+    cout << "arrPtr[2]: " << arrPtr[2] << endl;
+    cout << "arrPtr[3]: " << arrPtr[3] << endl;
+
+    cout << "*(arr): " << *(arr) << endl;
+    cout << "*(arr + 1): " << *(arr + 1) << endl;
+    cout << "*(arr + 2): " << *(arr + 2) << endl;
+    cout << "*(arr + 3): " << *(arr + 3) << endl;
+
+    cout << "*(arrPtr): " << *(arrPtr) << endl;
+    cout << "*(arrPtr + 1): " << *(arrPtr + 1) << endl;
+    cout << "*(arrPtr + 2): " << *(arrPtr + 2) << endl;
+    cout << "*(arrPtr + 3): " << *(arrPtr + 3) << endl;
+
+    return 0;
+}
