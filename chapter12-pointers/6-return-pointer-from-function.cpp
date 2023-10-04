4571b66e20277108bb2d577b2ef1c685bd6c6b65
lapeko <lapeko88@gmail.com>
2023-10-04 05:15:06 +0200

    improve example

--- chapter12-pointers/6-return-pointer-from-function.cpp
+++ chapter12-pointers/6-return-pointer-from-function.cpp
@@ -3,16 +3,22 @@
 using namespace std;
 
 int* createArray(int size, int initValue);
+void displayArray(const int* array, int size);
 
 int main() {
     const int size = 4;
-    const int* arr = createArray(size, 5);
-    for (int i = 0; i < size; i++)
-        cout << "arr[" << i << "]: " << arr[i] << endl;
+    const int* arrPtr = createArray(size, 5);
+    displayArray(arrPtr, size);
+    delete[] arrPtr;
 }
 
 int* createArray(int size, int initValue = 0) {
     int* array = new int[size];
     for (int i = size; i != 0; i--) array[i - 1] = initValue;
     return array;
+};
+
+void displayArray(const int* const array, int size) {
+    for (int i = 0; i < size; i++)
+        cout << "arr[" << i << "]: " << array[i] << endl;
 };
