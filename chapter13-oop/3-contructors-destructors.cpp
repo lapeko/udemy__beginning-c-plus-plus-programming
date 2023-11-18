73f631311a7875a70f3970935326802987db165c
lapeko <lapeko88@gmail.com>
2023-11-18 09:21:29 +0100

    refactoring

--- chapter13-oop/3-contructors-destructors.cpp
+++ chapter13-oop/3-contructors-destructors.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 using namespace std;
 
 class Player {
@@ -8,13 +9,13 @@ private:
     int xp;
 
 public:
-    void set_name(string new_name) { name = new_name; }
+    void set_name(string new_name) { name = std::move(new_name); }
     string get_name() { return name; }
 
     Player(): Player("None") { cout << "Player()" << endl; }
-    Player(string name): Player(name, 0) { cout << "Player(string name)" << endl; }
-    Player(string name, int age): Player(name, age, 0) { cout << "Player(string name, int age)" << endl; }
-    Player(string name_value, int age_value, int xp_value): name{name_value}, age{age_value}, xp{xp_value} {
+    explicit Player(string name): Player(std::move(name), 0) { cout << "Player(string name)" << endl; }
+    Player(string name, int age): Player(std::move(name), age, 0) { cout << "Player(string name, int age)" << endl; }
+    Player(string name_value, int age_value, int xp_value): name{std::move(name_value)}, age{age_value}, xp{xp_value} {
         cout << "Player(string name_value, int age_value, int xp_value)" << endl;
     }
 
@@ -31,7 +32,7 @@ int main() {
     }
 
     {
-        Player* player = new Player("Igor", 55);
+        auto* player = new Player("Igor", 55);
         delete player;
     }
     return 0;
