2af60164fb04b797cdcc2a16a1b60a83ad69a00f
lapeko <lapeko88@gmail.com>
2023-11-18 09:41:20 +0100

    learn copy constructor

--- chapter13-oop/3-contructors-destructors.cpp
+++ chapter13-oop/3-contructors-destructors.cpp
@@ -12,19 +12,35 @@ private:
 public:
     void set_name(string new_name) { name = std::move(new_name); }
 
-    string get_name() { return name; }
+    string get_name() const { return name; }
+    int get_age() const { return age; }
+    int get_xp() const { return xp; }
 
     explicit Player(string name_value = "None", int age_value = 0, int xp_value = 0) :
             name{std::move(name_value)},
             age{age_value},
             xp{xp_value} {
-        cout << "constructor. Creates name: " << name << endl;
+        cout << "constructor. Creates name: " << name << endl << endl;
+    }
+
+    Player(const Player &player) :
+            name{player.name},
+            age{player.age},
+            xp{player.xp} {
+        cout << "copy constructor: Name: " << player.name << endl << endl;
     }
 
     ~Player() {
-        cout << "~Player. name: " << name << endl;
+        cout << "~Player. name: " << name << endl << endl;
     }
 };
+
+void show_player_by_copy(const Player player) {
+    cout << "Player:" << endl;
+    cout << "name: " << player.get_name() << endl;
+    cout << "age: " << player.get_age() << endl;
+    cout << "xp: " << player.get_xp() << endl << endl;
+}
 
 int main() {
     {
@@ -37,5 +53,9 @@ int main() {
         auto *player = new Player("Igor", 55);
         delete player;
     }
+
+    Player player;
+    show_player_by_copy(player);
+
     return 0;
 }
