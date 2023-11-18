6853b316de48e44c732568ce0a5bea9d3509a623
lapeko <lapeko88@gmail.com>
2023-11-18 09:30:48 +0100

    use constructor with default parameters

--- chapter13-oop/3-contructors-destructors.cpp
+++ chapter13-oop/3-contructors-destructors.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <utility>
+
 using namespace std;
 
 class Player {
@@ -10,13 +11,14 @@ private:
 
 public:
     void set_name(string new_name) { name = std::move(new_name); }
+
     string get_name() { return name; }
 
-    Player(): Player("None") { cout << "Player()" << endl; }
-    explicit Player(string name): Player(std::move(name), 0) { cout << "Player(string name)" << endl; }
-    Player(string name, int age): Player(std::move(name), age, 0) { cout << "Player(string name, int age)" << endl; }
-    Player(string name_value, int age_value, int xp_value): name{std::move(name_value)}, age{age_value}, xp{xp_value} {
-        cout << "Player(string name_value, int age_value, int xp_value)" << endl;
+    explicit Player(string name_value = "None", int age_value = 0, int xp_value = 0) :
+            name{std::move(name_value)},
+            age{age_value},
+            xp{xp_value} {
+        cout << "constructor. Creates name: " << name << endl;
     }
 
     ~Player() {
@@ -32,7 +34,7 @@ int main() {
     }
 
     {
-        auto* player = new Player("Igor", 55);
+        auto *player = new Player("Igor", 55);
         delete player;
     }
     return 0;
