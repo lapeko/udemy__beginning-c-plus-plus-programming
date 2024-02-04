9f842ac8ad0452eac4215dfc9be16a1362833209
lapeko <lapeko88@gmail.com>
2024-02-04 13:17:35 +0100

    refactor challenge

--- chapter9/2-loops-challenge/main.cpp
+++ chapter9/2-loops-challenge/main.cpp
@@ -1,79 +1,112 @@
-#include <iostream>
 #include <vector>
-#include <cctype>
-#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <cstdlib>
 
 using namespace std;
 
+void show_menu();
+char handle_user_input();
+void print_numbers(vector<int>* numbers);
+void add_number(vector<int>* numbers);
+void print_mean_number(vector<int>* numbers);
+void print_smallest_number(vector<int>* numbers);
+void print_largest_number(vector<int>* numbers);
+void clear_console();
+
 int main() {
-    vector<int> ints {};
-    
+    bool quit_pressed {false};
+    vector<int> numbers {};
+    do {
+        show_menu();
+        const char user_input {handle_user_input()};
+        switch (user_input) {
+            case 'p': print_numbers(&numbers); break;
+            case 'a': add_number(&numbers); break;
+            case 'm': print_mean_number(&numbers); break;
+            case 's': print_smallest_number(&numbers); break;
+            case 'l': print_largest_number(&numbers); break;
+            case 'q': quit_pressed = true; break;
+            default: cout << "Unknown value. PLease, try again" << endl;
+        }
+    } while (!quit_pressed);
+}
+
+void show_menu() {
     cout << "P - Print numbers" << endl;
     cout << "A - Add a number" << endl;
     cout << "M - Display mean of the numbers" << endl;
     cout << "S - Display the smallest number" << endl;
     cout << "L - Display the largest number" << endl;
-    cout << "Q - Quit" << endl << endl;
-    
-    while(true) {
-        cout << "Enter your choice" << endl;
-        char userChoice;
-        cin >> userChoice;
-        userChoice = tolower(userChoice);
-        
-        if (userChoice == 'p') {
-            cout << "[";
-            for (int num: ints) cout << " " << num;
-            if (ints.size()) cout << " ";
-            cout << "]" << endl;
+    cout << "Q - Quit" << endl;
+}
+
+char handle_user_input() {
+    char user_input {};
+    cin >> user_input;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    clear_console();
+
+    return tolower(user_input);
+}
+
+void print_numbers(vector<int>* numbers) {
+    if (numbers->empty()) {
+        cout << "[] - this list is empty" << endl;
+        return;
+    };
+    cout << "[";
+    for (auto num : *numbers) cout << " " << num << " ";
+    cout << "]" << endl;
+};
+
+void add_number(vector<int>* numbers) {
+    cout << "Enter a number: ";
+    int new_number {};
+    while (!(cin >> new_number)) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "Invalid input. Please enter a number: ";
         }
-        
-        else if (userChoice == 'a') {
-            int newIntNumber {0};
-            cout << "Enter an integer number: ";
-            cin >> newIntNumber;
-            ints.push_back(newIntNumber);
-            cout << ints.at(ints.size() -1) << " added" << endl;
-        }
-        
-        else if (userChoice == 'm') {
-            if (!ints.size()) {
-                cout << "Unable to calculate the mean - no data" << endl;
-                continue;
-            }
-            float sum {0};
-            for (int num: ints) sum += num;
-            cout << fixed << setprecision(2);
-            cout << "The average number is: " << sum / ints.size() << endl;
-        }
-        
-         else if (userChoice == 's') {
-            if (!ints.size()) {
-                cout << "Unable to determine the smallest number - list is empty" << endl;
-                continue;
-            }
-            int smallestElement = ints.at(0);
-            for (int num: ints) if (num < smallestElement) smallestElement = num;
-            cout << "The smallest number is: " << smallestElement << endl;
-        }
-        
-        else if (userChoice == 'l') {
-            if (!ints.size()) {
-                cout << "Unable to determine the largest number - list is empty" << endl;
-                continue;
-            }
-            int smallestElement = ints.at(0);
-            for (int num: ints) if (num > smallestElement) smallestElement = num;
-            cout << "The smallest number is: " << smallestElement << endl;
-        }
-        
-        else if (userChoice == 'q'){
-            cout << "Exit";
-            break;
-        } else {
-            cout << "Unknown selection, please try again" << endl;
-        }
+    };
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    numbers->push_back(new_number);
+};
+
+void print_mean_number(vector<int>* numbers) {
+    if (numbers->empty()) {
+        cout << "Unable to calculate the mean" << endl;
+        return;
     }
-    
-    return 0;
+    int sum {};
+    for (auto num : *numbers) sum += num;
+    cout << "The mean of numbers is: " << static_cast<double>(sum) / numbers->size() << endl;
+};
+
+void print_smallest_number(vector<int>* numbers) {
+    if (numbers->empty()) {
+        cout << "Unable to determine the smallest number" << endl;
+        return;
+    }
+    int smallest_number = numbers->at(0);
+    for (auto num : *numbers)
+        if (num < smallest_number) smallest_number = num;
+    cout << "The smallest number is: " << smallest_number << endl;
+};
+
+void print_largest_number(vector<int>* numbers) {
+    if (numbers->empty()) {
+        cout << "Unable to determine the largest number" << endl;
+        return;
+    }
+    int biggest_number = numbers->at(0);
+    for (auto num : *numbers)
+        if (num > biggest_number) biggest_number = num;
+    cout << "The smallest number is: " << biggest_number << endl;
+};
+
+void clear_console() {
+    string environment = getenv("TERM");
+    environment == "xterm" ? system("clear") : system("cls");
 }
