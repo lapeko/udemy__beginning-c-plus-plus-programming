63a2cdd0af74cf3fd36334d515c6ad977246267c
lapeka.v <v.lapeka@andersenlab.com>
2024-02-17 01:18:06 +0100

    use shared pointers. use custom smart pointer deleter

---
+++ chapter17-smart-pointers/shared_pointer.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <memory>
+
+using namespace std;
+
+class Object {
+    int data;
+public:
+    explicit Object(int data = 0): data(data) {};
+    int get_data() const {return data;};
+};
+
+void custom_deleter(Object* object) {
+    cout << "Custom deleter" << endl;
+    delete object;
+}
+
+int main() {
+    vector<shared_ptr< Object> > objects;
+
+    {
+        shared_ptr<Object> ptr1 (new Object(20), custom_deleter);
+        shared_ptr<Object> ptr2 = ptr1;
+        objects.push_back(ptr1);
+        cout << objects[0].use_count() << endl;
+        ptr2.reset();
+        cout << objects[0].use_count() << endl;
+    }
+
+    cout << objects[0].use_count() << endl;
+
+    objects.clear();
+
+    cout << "==================================================" << endl;
+    return 0;
+}
