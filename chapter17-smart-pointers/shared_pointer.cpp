c3584e22c26ff52a53139ab55ebf6a306a83380b
lapeko <lapeko88@gmail.com>
2024-02-17 14:39:48 +0100

    use arrow function instead

--- chapter17-smart-pointers/shared_pointer.cpp
+++ chapter17-smart-pointers/shared_pointer.cpp
@@ -11,16 +11,14 @@ public:
     int get_data() const {return data;};
 };
 
-void custom_deleter(Object* object) {
-    cout << "Custom deleter" << endl;
-    delete object;
-}
-
 int main() {
     vector<shared_ptr< Object> > objects;
 
     {
-        shared_ptr<Object> ptr1 {new Object(20), custom_deleter};
+        shared_ptr<Object> ptr1 {new Object{20}, [] (Object* obj) {
+            cout << "Custom deleter" << endl;
+            delete obj;
+        }};
         shared_ptr<Object> ptr2 = ptr1;
         objects.push_back(ptr1);
         cout << objects[0].use_count() << endl;
