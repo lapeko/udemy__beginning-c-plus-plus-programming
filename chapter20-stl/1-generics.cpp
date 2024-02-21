6c47a8621e8583e8d4b1987c941cf5b4178f5f2e
lapeko <lapeko88@gmail.com>
2024-02-21 22:59:27 +0100

    play with template typename and value

--- chapter20-stl/1-generics.cpp
+++ chapter20-stl/1-generics.cpp
@@ -23,6 +23,41 @@ public:
     };
 };
 
+template<typename T>
+class Item {
+    string name;
+    T value;
+public:
+    Item(string name, T value): name{std::move(name)}, value{std::move(value)} {};
+    string get_name() {
+        return name;
+    };
+    T get_value() {
+        return value;
+    };
+};
+
+template<typename T, int N>
+class MyArray {
+    int size{N};
+    T values[N]{};
+    friend ostream& operator<<(ostream& os, const MyArray& myArray) {
+        os << "[MyArray] size: " << myArray.size << endl << "Elements: ";
+        for (int i = 0; i < myArray.size; i++)
+            os << myArray.values[i] << (i < myArray.size - 1 ? ", " : "");
+        os << endl;
+        return os;
+    };
+public:
+    T& operator[](int index) {
+        return values[index];
+    };
+    void fill(T value) {
+        for (int i = 0; i < size; i++)
+            values[i] = value;
+    };
+};
+
 int main() {
     // example 1 - generic based on define
     {
@@ -36,6 +71,18 @@ int main() {
         Person p2{"Katrin", 26};
         cout << "Min: " << min(p1, p2) << endl;
     }
+
+    // example 3
+    {
+        Item<Item<int>> outer{"Outer", {"Inner", 12}};
+        Item<int> inner = outer.get_value();
+        cout << "Outer name: " << outer.get_name() << ", inner name: " << inner.get_name() << ", inner value: " << inner.get_value() << endl;
+    }
+
+    // example 4 - custom array. overload operator []. template int value
+    MyArray<int, 5> myArray;
+    myArray.fill(5);
+    cout << myArray;
 
     return 0;
 }
